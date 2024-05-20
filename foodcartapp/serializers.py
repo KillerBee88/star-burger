cca1e966182767e400b453a2fbf9d28bb8c4fbe7
KillerBee88 <hardmen08@gmail.com>
2024-05-20 22:46:15 +0300

    Update serializers.py
    Send order data to frontend.

--- foodcartapp/serializers.py
+++ foodcartapp/serializers.py
@@ -1,16 +1,31 @@
 from rest_framework import serializers
+
 from .models import Order, OrderItem, Product
 
-class OrderItemSerializer(serializers.Serializer):
-    product = serializers.IntegerField()
-    quantity = serializers.IntegerField()
 
-class OrderSerializer(serializers.Serializer):
-    firstname = serializers.CharField(max_length=100)
-    lastname = serializers.CharField(max_length=100)
-    phonenumber = serializers.RegexField(regex=r'^\+79\d{9}$')
-    address = serializers.CharField(max_length=255)
-    products = OrderItemSerializer(many=True)
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['product', 'quantity']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    products = OrderItemSerializer(
+        many=True,
+        allow_empty=False,
+        write_only=True
+    )
+
+    class Meta:
+        model = Order
+        fields = [
+            'id',
+            'products',
+            'firstname',
+            'lastname',
+            'phonenumber',
+            'address'
+        ]
 
     def validate_products(self, value):
         if not value:
@@ -18,10 +33,12 @@ class OrderSerializer(serializers.Serializer):
 
         product_ids = [item['product'] for item in value]
         if len(product_ids) != len(set(product_ids)):
-            raise serializers.ValidationError("Duplicate products are not allowed")
-        
+            raise serializers.ValidationError(
+                "Duplicate products are not allowed")
+
         if not Product.objects.filter(id__in=product_ids).exists():
-            raise serializers.ValidationError("One or more products do not exist")
+            raise serializers.ValidationError(
+                "One or more products do not exist")
 
         return value
 
@@ -32,7 +49,7 @@ class OrderSerializer(serializers.Serializer):
             product = Product.objects.get(pk=product_data['product'])
             OrderItem.objects.create(
                 order=order,
-                product_name=product.name,
+                product=product,
                 quantity=product_data['quantity'],
                 price=product.price
             )
