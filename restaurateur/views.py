24126f202ee7a72dbb8cb5bb59fda759d000b306
KillerBee88 <hardmen08@gmail.com>
2024-05-23 22:24:12 +0300

    Update views.py
    Calculate total price for manager

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -93,4 +93,7 @@ def view_restaurants(request):
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
     orders = Order.objects.all()
+    for order in orders:
+        order.total_price_value = order.total_price()
+
     return render(request, template_name='order_items.html', context={'orders': orders})
